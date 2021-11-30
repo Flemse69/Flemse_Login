3cb465255f10c88cf58c2369a7b83843c050499a
Flemse69 <84687882+Flemse69@users.noreply.github.com>
2021-11-30 19:36:27 +0100

    Add files via upload

---
+++ oxide_spawnselect/__resource.lua
@@ -0,0 +1,17 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+    "client.lua"
+}
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/script.js',
+    'ui/style.css',
+    'ui/images/seneste.png',
+	'ui/images/midtby.png',
+    'ui/images/paleto.png',
+    'ui/images/sandy.png'
+}
